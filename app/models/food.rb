6ce68c26fefcb9adb238252be4e4a5c5d94b793f
oshiro <kaito.o.0628@gmail.com>
2022-08-07 13:38:38 +0900

    いいね機能のルーティング、コントローラを作成

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,13 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
+  # food.liked_usersでfoodを「いいね」したユーザー一覧を取得
+  has_many :liked_users, through: :likes, source: :user
   validates :name, presence: true
   mount_uploader :image, ImageUploader
+
+  # foodをuserが「いいね」しているときはtrue
+  def liked_by?(user)
+    likes.exists?(user_id: user.id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :foods, dependent: :destroy
   has_many :likes, dependent: :destroy
+  # user.liked_foodsでuserが「いいね」しているメッセージ一覧を取得
+  has_many :liked_foods, through: :likes, source: :food
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
